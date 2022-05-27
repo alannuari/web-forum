c6f7136b8117be186ad792d49175611bbff68c06
alannuari <alannuari372@gmail.com>
2022-05-27 16:19:48 +0800

    merapikan tampilan

--- src/components/Content.js
+++ src/components/Content.js
@@ -17,7 +17,7 @@ const Content = () => {
             <h1 className='content__title'>Lampu webcam tiba-tiba menyala sendiri tanpa membuka aplikasi webcam</h1>
             <p className='content__describe'>Mau tanya, akhir-akhir ini webcam sering nyla sendiri. Apakah ada yang tahu penyebabnya dan solusi untuk memperbaiki hal itu? Apakah ada kemungkinan laptop saya di-hack karena kasus terjadi tiap terkoneksi internet.</p>
 
-            <h1>Komentar</h1>
+            <h2>Komentar</h2>
             {data.length > 0 &&
                 data.map((item, idx) => {
                     return <Comment key={idx} data={item} />;
